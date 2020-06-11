0eaecbedf4df1a8f3ec8e8f150f9d0b505ed325d
silentstorm <animeshchakravertty@gmail.com>
2020-06-11 21:24:30 +0530

    Initial commit

---
+++ Basic_Calculator.py
@@ -0,0 +1,14 @@
+num1 = float(input("Enter the first number : "))
+num2 = float(input("Enter the second number : "))
+operation = (input("Enter the operation : "))
+if operation == "+":
+    result = num1 + num2
+elif operation == "-" :
+    result = num1 - num2
+elif operation == "/" :
+    result = num1 / num2
+elif operation == "*" :
+    result = num1 * num2
+else :
+    print("Invalid Input")
+print("The answer is" + "=" + str(result))
